9be250f818069bc2a8a97439847d2b0f05889244
Anupam <Anupam@DESKTOP-OAOU4N2>
2021-03-06 22:00:54 +0100

    updated

--- JavaTutorials/src/main/java/com/programs/HelloWorld.java
+++ JavaTutorials/src/main/java/com/programs/HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String newString = new String("anupam");
+		String x = "anupam";
+		System.out.println(newString==x);
 
 	}
 
